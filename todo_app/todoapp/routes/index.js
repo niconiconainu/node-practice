9de651ff1eed642a6161f47b76f0f7a31fefb1bb
Kyosuke Kaneko <kyosuke.soccer118@gmail.com>
2022-11-20 11:32:49 +0900

    Done:chpater13

--- todo_app/todoapp/routes/index.js
+++ todo_app/todoapp/routes/index.js
@@ -1,19 +1,48 @@
 const express = require('express');
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: '127.0.0.1',
+  user: 'root',
+  password: '',
+  database: 'todo_app',
+});
+
 const router = express.Router();
 
 let todos = [];
 
 router.get('/', function (req, res, next) {
-  res.render('index', {
-    title: 'ToDo App',
-    todos: todos,
-  });
+  connection.query(
+    `select * from tasks;`,
+    (error, results) => {
+      console.log(error);
+      console.log(results);
+      res.render('index', {
+        title: 'ToDo App',
+        todos: results,
+      });
+    }
+  );
 });
 
 router.post('/', function (req, res, next) {
+  connection.connect((err) => {
+    if (err) {
+      console.log('error connecting: ' + err.stack);
+      return
+    }
+    console.log('success');
+  });
   const todo = req.body.add;
-  todos.push(todo);
-  res.redirect('/');
+  connection.query(
+    `insert into tasks (user_id, content) values (1, '${todo}');`,
+    (error, results) => {
+      console.log(error);
+      res.redirect('/');
+    }
+  );
 });
 
 module.exports = router;
+
